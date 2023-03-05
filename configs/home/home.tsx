e5eb0a0a76ea92a3ff8539542338f38312496ee5
cs_qiuzz <qiuzz@uchonor.com>
2023-03-05 18:55:57 +0800

    增加首页 json 初始化配置

---
+++ configs/home/home.tsx
@@ -0,0 +1,18 @@
+let homeJson = {
+    "type": "image",
+    "src": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395692722/4f3cb4202335.jpeg@s_0,w_216,l_1,f_jpg,q_80"
+    // "type": "page",
+    // "body": {
+      /* "type": "carousel",
+      "auto": false,
+      "thumbMode": "cover",
+      "animation": "slide",
+      "height": 300,
+      "options": [
+        {
+          "image": "./configs/home/carouse1.png"
+        },
+      ] */
+    // }
+  }
+export default homeJson
