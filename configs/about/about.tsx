6921471d56de76d7779e7bc781e0425f916d4455
cs_qiuzz <qiuzz@uchonor.com>
2023-03-14 23:06:05 +0800

    增加关于我们，公司简介信息

--- configs/about/about.tsx
+++ configs/about/about.tsx
@@ -9,19 +9,126 @@ let aboutJson = {
         "innerClassName": "no-border"
       },
       {
-        "type": "tabs",
-        "tabsMode": "vertical",
+        "type": "flex",
+        "justify": "start",
+        "alignItems": "start",
         "style": {
-          "background": "linear-gradient(138deg, #4783E9 0%, #46BEE6 100%, #4EC1FE 100%)"
+          "paddingLeft": "160px"
         },
-        "tabs": [
+        "items": [
           {
-            "title": "公司简介",
-            "body": "选项卡内容1"
+            "type": "wrapper",
+            "width": "188px",
+            "height": "130px",
+            "style": {
+              "background": "linear-gradient(138deg, #4783E9 0%, #46BEE6 100%, #4EC1FE 100%)",
+              "boxShadow": "0px 2px 10px 0px rgba(0,0,0,0.3)",
+              "borderRadius": "10px",
+              "position": "relative",
+              "top": "-22px",
+              "padding": "30px 22px",
+              "color": "#fff",
+              "fontWeight": 500,
+              "fontSize": "14px",
+              "minWidth": "188px"
+            },
+            "body": [
+              {
+                "type": "flex",
+                "direction": "column",
+                "alignItems": "start",
+                "items": [{
+                  "type": "flex",
+                  "style": {
+                    "marginBottom": "26px"
+                  },
+                  "items": [
+                    {
+                      "type": "tpl",
+                      "tpl": "公司简介",
+                      "style": {
+                        "marginRight": "60px"
+                      }
+                    },
+                    {
+                      "type": "wrapper",
+                      "style": {
+                        "width": '20px',
+                        "height": "2px",
+                        "background": "#fff",
+                        "padding": 0
+                      }
+                    }
+                  ]
+                },
+                {
+                  "type": "flex",
+                  "style": {
+                    "marginBottom": "26px"
+                  },
+                  "items": [
+                    {
+                      "type": "tpl",
+                      "tpl": "荣誉资质",
+                      "style": {
+                        "marginRight": "60px"
+                      }
+                    },
+                    /* {
+                      "type": "wrapper",
+                      "style": {
+                        "width": '20px',
+                        "height": "2px",
+                        "background": "#fff",
+                        "padding": 0
+                      }
+                    } */
+                  ]
+                }]
+              }
+            ]
           },
           {
-            "title": "荣誉资质",
-            "body": "选项卡内容2"
+            "type": "wrapper",
+            "className": "text-info-wrapper",
+            "style": {
+              "padding": "20px 160px 18px 36px "
+            },
+            "body": [
+              {
+                "type": "tpl",
+                "style": {
+                  "fontSize": "32px",
+                  "fontWeight": 600,
+                  "color": "#333",
+                },
+                "tpl": "碳和科技（北京）有限公司",
+              },
+              {
+                "type": "tpl",
+                "tpl": "<p>碳和科技（北京）有限公司，位于北京市北京经济技术开发区（通州），拥有近三十年PSA行业经验，以二氧化碳捕集回收应用为出发点，开发了二氧化碳捕集利用、煤层气、甲烷等温室气体的治理和分离以及CO、H2等工业尾气的回收利用等多种气体净化回收技术，集技术研发、工程设计、设备制造、安装调试、EPC总包、EP承包及技术优化升级改造服务于一体。</p>"
+              },
+              {
+                "type": "wrapper",
+                "style": {
+                  "textAlign": "center"
+                },
+                "body": {
+                  "type": "image",
+                  "src": "../../assets/about/aboutInfo.png",
+                  "height": 280,
+                  "width": 600,
+                  "innerClassName": "no-border"
+                }
+              },
+              {
+                "type": "tpl",
+                "tpl": `<p>碳和科技属于北京市飞达捷能气体分离技术有限公司控股子公司，公司以研发为基石，与国内多所高校和科研院所建立了紧密的研发合作关系，先后取得了四十余项国家专利，研发生产出烟气提纯二氧化碳、变压吸附法制氮、制氧、制氢，煤层气提纯甲烷，以及气体净化干燥等多种工艺及装备，拥有自主知识产权的核心吸附材料，具备中试及产业化所需的工艺开发、设备制造、分析检测等条件，拥有较强的研发成果转化能力和丰富的工程化经验。
+                公司配备一、二类压力容器设备设计、制造基地及实验中试基地；拥有完善的质保体系，并取得ISO9001质量管理体系认证、ISO14001管理体系认证、ISO45001职业健康体系认证，以及CCS船级社认证和MA煤安标志认证。拥有先进的机械加工设备、完善的检测手段、科学的管理体系和专业化的研发队伍；拥有紧密型战略合作的甲级工程设计院，为用户的新建生产装置或老装置的技术改造量身定制；提供项目技术方案、立项可研、技术咨询、工程设计、设备加工、培训、调试等一体化服务。
+                未来，公司继续积极响应政府号召，把握发展机遇，以“让工业气体更安全、更节能、更洁净”为使命，致力于打造成为国内工业气体综合解决方案的服务商！
+                </p>`
+              }
+            ]
           }
         ]
       }
