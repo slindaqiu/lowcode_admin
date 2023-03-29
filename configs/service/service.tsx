a4a9d4be6f3f829caa13737e8a77de0746e8e1a5
cs_qiuzz <qiuzz@uchonor.com>
2023-03-29 23:28:06 +0800

    获取技术与服务导航左侧列表信息

--- configs/service/service.tsx
+++ configs/service/service.tsx
@@ -1,5 +1,39 @@
+/* 
+  根据类型获取子菜单列表
+*/
+const getSubNav = (title: any) => {
+  return {
+    "type": "nav",
+    "stacked": true,
+    // "source": "${nav}"
+    "source": {
+      "method": "get",
+      "url": "/news/list",
+      "data": {
+        "catalog": title
+      },
+      "adaptor": function(payload: any) {
+        let tempResult:any[] = []
+        if (payload.data && payload.data.items) {
+          payload.data.items.forEach((item:any) => {
+            tempResult.push({
+              "label": item.title,
+              "to": "?id=" + item.id
+            })
+          })
+        }
+        return {
+          status: payload.status,
+          msg: payload.msg,
+          data: tempResult
+        }
+      }
+    }
+  }
+}
 let serviceJson = {
-  "type": "service",
+  "type": "page",
+  // "initApi": "/news/info?id=${id}",
   "body": [
     {
       "type": "image",
@@ -9,8 +43,8 @@ let serviceJson = {
       "innerClassName": "no-border"
     },
     {
-      "type": "service",
-      "data": {
+      "type": "wrapper",
+      /* "data": {
         "nav": [
           {
             "label": "CO2精制提纯技术",
@@ -33,7 +67,7 @@ let serviceJson = {
             "to": "?title=石灰窑气、烟道气等碳捕集利用技术"
           }
         ]
-      },
+      }, */
       "body": {
         "type": "flex",
         "justify": "start",
@@ -54,56 +88,54 @@ let serviceJson = {
                 "type": "collapse",
                 "key": "1",
                 "header": "二氧化碳的捕集利用",
-                "body": {
-                  "type": "nav",
-                  "stacked": true,
-                  "source": "${nav}"
-                }
+                "body": getSubNav("二氧化碳的捕集利用")
               },
               {
                 "type": "collapse",
                 "key": "2",
                 "header": "变压吸附提纯CO技术",
-                "body": "这里是内容2"
+                "body": getSubNav("变压吸附提纯CO技术")
               },
               {
                 "type": "collapse",
                 "key": "3",
                 "header": "变压吸附制氢技术",
-                "body": "这里是内容3"
+                "body": getSubNav("变压吸附制氢技术")
               },
               {
                 "type": "collapse",
                 "key": "4",
                 "header": "甲烷提浓技术",
-                "body": "这里是内容1"
+                "body": getSubNav("甲烷提浓技术")
               },
               {
                 "type": "collapse",
                 "key": "5",
                 "header": "空分PSA制氮、制氧技术",
-                "body": "这里是内容2"
+                "body": getSubNav("空分PSA制氮、制氧技术")
               },
               {
                 "type": "collapse",
                 "key": "6",
                 "header": "气体干燥净化技术",
-                "body": "这里是内容3"
+                "body": getSubNav("气体干燥净化技术")
               },
               {
                 "type": "collapse",
                 "key": "7",
                 "header": "吸附剂、催化剂",
-                "body": "这里是内容3"
+                "body": getSubNav("吸附剂、催化剂")
               }
             ]
           },
           {
-            "type": "wrapper",
+            "type": "page",
+            "initApi": "/news/info?id=${id}",
             "body": {
               "type": "tpl",
-              "tpl": "内容"
+              "tpl": "${title}"
             }
+
           }
         ]
       }
